13ca4f183beafc2692cb6bc2aeb22dada0aa809d
Iqbalabdi <iqbalabdi07@gmail.com>
2022-05-12 21:22:17 +0700

    feat: add solution

--- rivercross.py
+++ rivercross.py
@@ -12,10 +12,10 @@ class RiverCrossing:
     self.right = right
     self.boat = boat
     self.children = children
- 
+
   def __str__(self):
     return str(self.left) + str(self.right) + ("Left" if not self.boat else "Right")
-  
+
   def ChildGenerator (self, prev, parent_map):
     children = []
     if not self.boat:
@@ -39,18 +39,18 @@ class RiverCrossing:
                 new_left.append(x)
                 new_right = self.right[:]
                 new_right.remove(x)
-  
+
                 if sorted(new_right) not in RiverCrossing.GameOver and not RiverCrossing.prev_state(prev, new_left, new_right, not self.boat):
                     child = RiverCrossing(new_left, new_right, not self.boat, [])
                     children.append(child)
                     parent_map[child] = self
-  
+
       if sorted(self.right) not in RiverCrossing.GameOver and not RiverCrossing.prev_state(prev, self.left[:], self.right[:], not self.boat):
                 child = RiverCrossing(self.left[:], self.right[:], not self.boat, [])
                 children.append(child)
                 parent_map[child] = self
     self.children = children
-  
+
   @staticmethod
   def prev_state(prev, left, right, boat):
         return any(
@@ -60,7 +60,22 @@ class RiverCrossing:
             for x in prev
         )
 
-    
+def find_solution(root, isDFS=False):
+    '''
+    Find a solution to the River Crossing Problem.
+    isDFS: True for DFS, False for BFS
+    '''
+    visit = [root]
+    node = root
+    prev = []
+    parent_map = {root: None}
+
+    while visit:
+        node = visit.pop()
+        if not RiverCrossing.prev_state(prev, node.left, node.right, node.boat):
+            prev.append(node)
+        node.ChildGenerator(prev, parent_map)
+
 
 print("Choose Method:")
 print("1. DFS")
@@ -77,7 +92,7 @@ if (menu):
         print("Steps #" + str(count) + ":")
         print(i, '\b')
         print("\n")
-else:    
+else:
     solution = find_solution(root, isDFS=True)
     print("DFS solution = \n")
     for i in solution:
