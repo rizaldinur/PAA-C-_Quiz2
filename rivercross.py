8b7369e7a8d8fdc16d7d5294f6543a13a33a8051
VickyThirdian <54606856+thirdianv@users.noreply.github.com>
2022-05-12 20:31:16 +0700

    add childgenerator func

--- rivercross.py
+++ rivercross.py
@@ -16,8 +16,40 @@ class RiverCrossing:
   def __str__(self):
     return str(self.left) + str(self.right) + ("Left" if not self.boat else "Right")
   
+  def ChildGenerator (self, prev, parent_map):
+    children = []
+    if not self.boat:
+      for x in self.left:
+        new_left = self.left[:]
+        new_right = self.right[:]
+        new_left.remove(x)
+        new_right.append(x)
 
- 
+        if sorted(new_left) not in RiverCrossing.GameOver and not RiverCrossing.prev_state(prev, new_left, new_right, not self.boat):
+                    child = RiverCrossing(new_left, new_right, not self.boat, [])
+                    children.append(child)
+                    parent_map[child] = self
+      if sorted(self.left) not in RiverCrossing.GameOver and not RiverCrossing.prev_state(prev, self.left[:], self.right[:], not self.boat):
+                child = RiverCrossing(self.left[:], self.right[:], not self.boat, [])
+                children.append(child)
+                parent_map[child] = self
+    else:
+      for x in self.right:
+                new_left = self.left[:]
+                new_left.append(x)
+                new_right = self.right[:]
+                new_right.remove(x)
+  
+                if sorted(new_right) not in RiverCrossing.GameOver and not RiverCrossing.prev_state(prev, new_left, new_right, not self.boat):
+                    child = RiverCrossing(new_left, new_right, not self.boat, [])
+                    children.append(child)
+                    parent_map[child] = self
+  
+      if sorted(self.right) not in RiverCrossing.GameOver and not RiverCrossing.prev_state(prev, self.left[:], self.right[:], not self.boat):
+                child = RiverCrossing(self.left[:], self.right[:], not self.boat, [])
+                children.append(child)
+                parent_map[child] = self
+    self.children = children
   
   @staticmethod
   def prev_state(prev, left, right, boat):
